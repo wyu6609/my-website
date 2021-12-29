1273e8670edc8198a653f198acb31150cc79637c
William Yu <wy427@nyu.edu>
2021-12-29 15:36:15 -0500

    added modal

---
+++ modal.js
@@ -0,0 +1,24 @@
+<div className="modal-bg">
+  <div className="modal">
+    <form action="/action_page.php" target="_blank">
+      <div class="w3-section">
+        <label>Name</label>
+        <input class="w3-input w3-border" type="text" name="Name" required />
+      </div>
+      <div class="w3-section">
+        <label>Email</label>
+        <input class="w3-input w3-border" type="text" name="Email" required />
+      </div>
+      <div class="w3-section">
+        <label>Message</label>
+        <input class="w3-input w3-border" type="text" name="Message" required />
+      </div>
+      <button
+        type="submit"
+        class="w3-button w3-block w3-padding-large w3-red w3-margin-bottom"
+      >
+        Send Message
+      </button>
+    </form>
+  </div>
+</div>;
